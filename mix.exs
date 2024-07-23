fe7bda8def380bb30c968e03fcd3defa0008c8f2
Fatigue <fatigue.dev@gmail.com>
2024-07-23 22:01:02 +1000

    Upgrade bcrypt

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule VsGlobalChat.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:bcrypt_elixir, "~> 3.0"},
+      {:bcrypt_elixir, "~> 3.1.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
